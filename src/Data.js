ce8aa213c29a5301e9ca7e395e46ebecf87008be
TStopher <115030113+TStopher@users.noreply.github.com>
2023-01-16 13:07:26 -0700

    complete overhaul
    added img exports, filled descriptions for style purposes, changed "rating" to "platforms"

--- src/Data.js
+++ src/Data.js
@@ -1,30 +1,40 @@
    import img1 from "./React-Project-img/Atomic-Heart-img.jpg"
-    
-    let Data =  [
+   import img2 from "./React-Project-img/wo-long-fallen-dynasty-jpg.jpg"
+   import img3 from "./React-Project-img/RE4-jpg.jpg"
+   import img4 from "./React-Project-img/Street-Fighter-6.jpg"
+   let Data =  [
     {
       id: 1,
-      title: "Atomic Hearts",
+      title: "Atomic Heart",
       coverImg: img1,
-      releaseDate: "NA",
-      platforms: "NA",
-      description: "NA"
+      releaseDate: "Releases on: Feb 21",
+      platforms: "Platforms: PS4/5, Xbox x|s, Xbox One, PC",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     },
     {
         id: 2,
-        title: "game2",
-        coverImg: "NA",
-        releaseDate: "NA",
-        platforms: "NA",
-        description: "NA"
+        title: "Wo Long:Fallen Dynasty",
+        coverImg: img2,
+        releaseDate: "Releases on: March 3",
+        platforms: "Platforms: PS4/5, Xbox x|s, Xbox One, PC",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
       },
       {
         id: 3,
-        title: "game3",
-        coverImg: "NA",
-        releaseDate: "NA",
-        platforms: "NA",
-        description: "NA",
-      }
+        title: "Resident Evil 4 Remake",
+        coverImg: img3,
+        releaseDate: "Releases on: March 24",
+        platforms:"Platforms: PS4/5, Xbox x|s, Xbox One, PC" ,
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      },
+      {
+        id:4,
+        title:"Street Fighter 6",
+        coverImg: img4,
+        releaseDate: "Releases on: June 2",
+        platforms:"Platforms: PS4/5, Xbox x|s, Xbox One, PC",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      }, 
    ]
 
 export default Data;
